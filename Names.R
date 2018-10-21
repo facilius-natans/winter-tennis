e2708a8f814518db3bdd29f740d2a509ff13435c
facilius-natans <facilius.natans@gmail.com>
2018-10-21 19:19:07 -0400

    Add valid doubles generator.

--- Names.R
+++ Names.R
@@ -17,11 +17,41 @@ print(typeof(goodPairNumbers))
 test2 <- combn(goodPairNumbers, m=rows, fun=NULL, simplify=TRUE)
 
 #print(test2)
-print(test2[,7140])
-print(test2[,6140])
-print(length(test2))
+# print(test2[,7140])
+# print(test2[,6140])
+# print(length(test2))
+# 
+# print(test2[,7140][1])
+# print(test2[,7140][2])
+# print(pairs[,test2[,7140][2]])
+# print(pairs[,test2[,7140][1]])
+ 
+valid_doubles <- c()   
+#loop through all the paira of pairs
+for(i in 1:(length(test2)/2)) {
+  # print(paste("printing pair:", i))
 
-print(test2[,7140][1])
-print(test2[,7140][2])
-print(pairs[,test2[,7140][2]])
+  pair1 <- pairs[,test2[,i][1]]
+  # print(pair1[1])
+  # print(pair1[2])
+  pair2 <- pairs[,test2[,i][2]]
+  # print(pair2[1])
+  # print(pair2[2])
+  if(pair1[1] != pair2[1] && pair1[1] != pair2[2] && pair1[2] != pair2[1] && pair1[2] != pair2[2]) {
+    valid_doubles <- c(valid_doubles, i)
+  }
+}
+print(length(valid_doubles))
+
+for(i in 1:(length(valid_doubles))) {
+  print(paste("printing pair:", i))
+  pair1 <- pairs[,test2[,i][1]]
+  print(pair1[1])
+  print(pair1[2])
+  pair2 <- pairs[,test2[,i][2]]
+  print(pair2[1])
+  print(pair2[2])
+}
+
+
 
